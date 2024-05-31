f04ec45832eb4944837015683ddaa79fb5929ba9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-05-31 22:11:57 +0000

    chore(deps): update terraform-module

--- examples/complete-rotation-policy/main.tf
+++ examples/complete-rotation-policy/main.tf
@@ -37,7 +37,7 @@ module "resource_group" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.13.3"
+  version              = "1.13.4"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
@@ -51,7 +51,7 @@ module "secrets_manager" {
 module "iam_secrets_engine" {
   count                = var.existing_sm_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager-iam-engine/ibm"
-  version              = "1.2.0"
+  version              = "1.2.1"
   region               = var.region
   secrets_manager_guid = module.secrets_manager.secrets_manager_guid
   iam_engine_name      = "generated_iam_engine"
